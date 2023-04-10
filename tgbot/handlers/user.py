b7902f2159c09a4b0da7387a03e34aa04444ed11
unknown <artemon.m174@gmail.com>
2023-04-10 23:48:43 +0500

    Добавлено главное меню

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -2,6 +2,7 @@ from aiogram import Dispatcher
 from aiogram.types import Message
 from ..keyboards.reply import *
 
+
 async def user_start(message: Message):
     await message.reply(f"Привет, {message.from_user.first_name}!\nВыбери то что тебе нужно в меню ниже", reply_markup=main_menu)
 
